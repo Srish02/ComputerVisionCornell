2cd398087c315b34e64089aa7093b92c757cc809
Srish02 <srgupta290@gmail.com>
2022-03-21 20:06:11 -0400

    Add files via upload

---
+++ project2coffee_and_donuts/README.md
@@ -0,0 +1,2 @@
+# project-2-feature-detection
+Project Page: https://www.cs.cornell.edu/courses/cs5670/2021sp/projects/pa2/
---
+++ project2coffee_and_donuts/features_scale_invariant.py
@@ -0,0 +1,529 @@
+import math
+
+import cv2
+import numpy as np
+import scipy
+from scipy import ndimage, spatial
+
+import transformations
+
+
+def inbounds(shape, indices):
+    assert len(shape) == len(indices)
+    for i, ind in enumerate(indices):
+        if ind < 0 or ind >= shape[i]:
+            return False
+    return True
+
+
+## Keypoint detectors ##########################################################
+
+class KeypointDetector(object):
+    def detectKeypoints(self, image):
+        '''
+        Input:
+            image -- uint8 BGR image with values between [0, 255]
+        Output:
+            list of detected keypoints, fill the cv2.KeyPoint objects with the
+            coordinates of the detected keypoints, the angle of the gradient
+            (in degrees), the detector response (Harris score for Harris detector)
+            and set the size to 10.
+        '''
+        raise NotImplementedError()
+
+
+class DummyKeypointDetector(KeypointDetector):
+    '''
+    Compute silly example features. This doesn't do anything meaningful, but
+    may be useful to use as an example.
+    '''
+
+    def detectKeypoints(self, image):
+        '''
+        Input:
+            image -- uint8 BGR image with values between [0, 255]
+        Output:
+            list of detected keypoints, fill the cv2.KeyPoint objects with the
+            coordinates of the detected keypoints, the angle of the gradient
+            (in degrees), the detector response (Harris score for Harris detector)
+            and set the size to 10.
+        '''
+        image = image.astype(np.float32)
+        image /= 255.
+        features = []
+        height, width = image.shape[:2]
+
+        for y in range(height):
+            for x in range(width):
+                r = image[y, x, 0]
+                g = image[y, x, 1]
+                b = image[y, x, 2]
+
+                if int(255 * (r + g + b) + 0.5) % 100 == 1:
+                    # If the pixel satisfies this meaningless criterion,
+                    # make it a feature.
+
+                    f = cv2.KeyPoint()
+                    f.pt = (x, y)
+                    # Dummy size
+                    f.size = 10
+                    f.angle = 0
+                    f.response = 10
+
+                    features.append(f)
+
+        return features
+
+
+class HarrisKeypointDetector(KeypointDetector):
+
+    # Compute harris values of an image.
+    def computeHarrisValues(self, srcImage):
+        '''
+        Input:
+            srcImage -- Grayscale input image in a numpy array with
+                        values in [0, 1]. The dimensions are (rows, cols).
+        Output:
+            harrisImage -- numpy array containing the Harris score at
+                           each pixel.
+            orientationImage -- numpy array containing the orientation of the
+                                gradient at each pixel in degrees.
+        '''
+        height, width = srcImage.shape[:2]
+
+        harrisImage = np.zeros(srcImage.shape[:2])
+        orientationImage = np.zeros(srcImage.shape[:2])
+
+        # TODO 1: Compute the harris corner strength for 'srcImage' at
+        # each pixel and store in 'harrisImage'.  See the project page
+        # for direction on how to do this. Also compute an orientation
+        # for each pixel and store it in 'orientationImage.'
+        # TODO-BLOCK-BEGIN
+
+        #Apply a Gaussian filter to smooth out any noise
+        # srcImage = cv2.GaussianBlur(srcImage,(5,5),0)
+
+        sobX = ndimage.sobel(srcImage, 0)
+        sobY = ndimage.sobel(srcImage, 1)
+
+        Ix2 = sobX * sobX
+        Ixy = sobX * sobY
+        Iy2 = sobY * sobY
+        
+        Au = ndimage.gaussian_filter(Ix2, 0.5)
+        Buv = ndimage.gaussian_filter(Ixy, 0.5)
+        Cv = ndimage.gaussian_filter(Iy2, 0.5)
+
+        harris = ((Au * Cv) - (Buv ** 2))
+        trace = 0.1 * (Au + Cv) ** 2
+
+        harrisImage = harris - trace
+
+        degrees = np.arctan2(sobX, sobY)
+
+        orientationImage = np.degrees(degrees)
+
+        return harrisImage, orientationImage
+
+    def computeLocalMaxima(self, harrisImage):
+        '''
+        Input:
+            harrisImage -- numpy array containing the Harris score at
+                           each pixel.
+        Output:
+            destImage -- numpy array containing True/False at
+                         each pixel, depending on whether
+                         the pixel value is the local maxima in
+                         its 7x7 neighborhood.
+        '''
+        destImage = np.zeros_like(harrisImage, np.bool)
+
+        # TODO 2: Compute the local maxima image
+        # TODO-BLOCK-BEGIN
+        
+        w = harrisImage.shape[0]
+        h = harrisImage.shape[1]
+
+        temp = ndimage.maximum_filter(harrisImage, size=7)
+
+        for y in range(w):
+            for x in range(h):
+                if(temp[y,x] == harrisImage[y,x]):
+                    destImage[y,x] = True
+                else:
+                    destImage[y,x] = False
+        # TODO-BLOCK-END
+
+        return destImage
+
+    def detectKeypoints(self, image):
+        '''
+        Input:
+            image -- BGR image with values between [0, 255]
+        Output:
+            list of detected keypoints, fill the cv2.KeyPoint objects with the
+            coordinates of the detected keypoints, the angle of the gradient
+            (in degrees), the detector response (Harris score for Harris detector)
+            and set the size to 10.
+        '''
+        image = image.astype(np.float32)
+        image /= 255.
+        height, width = image.shape[:2]
+        features = []
+
+        # Create grayscale image used for Harris detection
+        grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # computeHarrisValues() computes the harris score at each pixel
+        # position, storing the result in harrisImage.
+        # You will need to implement this function.
+        harrisImage, orientationImage = self.computeHarrisValues(grayImage)
+
+        # Compute local maxima in the Harris image.  You will need to
+        # implement this function. Create image to store local maximum harris
+        # values as True, other pixels False
+        harrisMaxImage = self.computeLocalMaxima(harrisImage)
+
+        # Loop through feature points in harrisMaxImage and fill in information
+        # needed for descriptor computation for each point.
+        # You need to fill x, y, and angle.
+        for y in range(height):
+            for x in range(width):
+                if not harrisMaxImage[y, x]:
+                    continue
+
+                f = cv2.KeyPoint()
+
+                # TODO 3: Fill in feature f with location and orientation
+                # data here. Set f.size to 10, f.pt to the (x,y) coordinate,
+                # f.angle to the orientation in degrees and f.response to
+                # the Harris score
+                # TODO-BLOCK-BEGIN
+                f.size = 10
+                f.pt = (x,y)
+                f.angle = orientationImage[y,x]
+                f.response = harrisImage[y,x]
+
+                features.append(f)
+
+        return features
+
+
+class ORBKeypointDetector(KeypointDetector):
+    def detectKeypoints(self, image):
+        '''
+        Input:
+            image -- uint8 BGR image with values between [0, 255]
+        Output:
+            list of detected keypoints, fill the cv2.KeyPoint objects with the
+            coordinates of the detected keypoints, the angle of the gradient
+            (in degrees) and set the size to 10.
+        '''
+        detector = cv2.ORB_create()
+        # import pdb; pdb.set_trace()
+        return detector.detect(image)
+
+
+## Feature descriptors #########################################################
+
+
+class FeatureDescriptor(object):
+    # Implement in child classes
+    def describeFeatures(self, image, keypoints):
+        '''
+        Input:
+            image -- BGR image with values between [0, 255]
+            keypoints -- the detected features, we have to compute the feature
+            descriptors at the specified coordinates
+        Output:
+            Descriptor numpy array, dimensions:
+                keypoint number x feature descriptor dimension
+        '''
+        raise NotImplementedError
+
+
+class SimpleFeatureDescriptor(FeatureDescriptor):
+    # TODO: Implement parts of this function
+    def describeFeatures(self, image, keypoints):
+        '''
+        Input:
+            image -- BGR image with values between [0, 255]
+            keypoints -- the detected features, we have to compute the feature
+                         descriptors at the specified coordinates
+        Output:
+            desc -- K x 25 numpy array, where K is the number of keypoints
+        '''
+        image = image.astype(np.float32)
+        image /= 255.
+        grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        desc = np.zeros((len(keypoints), 5 * 5))
+
+        # pad img to center the feature point
+        padded = np.pad(grayImage, 2, mode='constant', constant_values=0)
+
+        for i, f in enumerate(keypoints):
+            x, y = int(f.pt[0]), int(f.pt[1])
+
+            # TODO 4: The simple descriptor is a 5x5 window of intensities
+            # sampled centered on the feature point. Store the descriptor
+            # as a row-major vector. Treat pixels outside the image as zero.
+            five_window = padded[y:y+5,x:x+5].reshape(-1)
+
+            desc[i] = five_window 
+            
+        return desc
+
+
+class MOPSFeatureDescriptor(FeatureDescriptor):
+    # TODO: Implement parts of this function
+    def describeFeatures(self, image, keypoints):
+        '''
+        Input:
+            image -- BGR image with values between [0, 255]
+            keypoints -- the detected features, we have to compute the feature
+            descriptors at the specified coordinates
+        Output:
+            desc -- K x W^2 numpy array, where K is the number of keypoints
+                    and W is the window size
+        '''
+        image = image.astype(np.float32)
+        image /= 255.
+        # This image represents the window around the feature you need to
+        # compute to store as the feature descriptor (row-major)
+        windowSize = 8
+        desc = np.zeros((len(keypoints), windowSize * windowSize))
+        grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        grayImage = ndimage.gaussian_filter(grayImage, 0.5)
+
+        for i, f in enumerate(keypoints):
+            # TODO 5: Compute the transform as described by the feature
+            # location/orientation. You will need to compute the transform
+            # from each pixel in the 40x40 rotated window surrounding
+            # the feature to the appropriate pixels in the 8x8 feature
+            # descriptor image.
+            transMx = np.zeros((2, 3))  
+
+            # getting the coordinates of the points
+            x, y = f.pt
+
+            # TODO5: Figure out what goes into these functions
+            # shift to origin
+            t1_mx = transformations.get_trans_mx(np.array((-x, -y,0)))
+            # because we are in 2d so only need z-angle
+            r_mx = transformations.get_rot_mx(0, 0, np.radians(-f.angle))
+            # scale from 40x40 to 8x8
+            s_mx = transformations.get_scale_mx(0.2, 0.2, 0)
+            # 
+            t2_mx = transformations.get_trans_mx(np.array((windowSize//2,windowSize//2,0)))
+            combined = np.dot(t2_mx, np.dot(s_mx, np.dot(r_mx, t1_mx)))
+            # combined is: [[ 0.2  0.   0.  -8.8]
+            # [ 0.   0.2  0.   3.8]
+            # [ 0.   0.   1.   0. ]
+            # [ 0.   0.   0.   1. ]] 
+            # we need the non-zero/one elements
+            transMx = combined[:2,[0,1,3]]
+
+            # Call the warp affine function to do the mapping
+            # It expects a 2x3 matrix
+            destImage = cv2.warpAffine(grayImage, transMx,
+                (windowSize, windowSize), flags=cv2.INTER_LINEAR)
+
+            # TODO 6: Normalize the descriptor to have zero mean and unit 
+            # variance. If the variance is negligibly small (which we 
+            # define as less than 1e-10) then set the descriptor
+            # vector to zero. Lastly, write the vector to desc.
+            normImg = destImage - destImage.mean()
+            var = np.std(destImage)
+            if(var < 1e-5):
+                desc[i] = np.zeros(np.square(windowSize))
+            else:
+                desc[i] = (normImg/var).reshape(np.square(windowSize))
+        return desc
+
+
+class ORBFeatureDescriptor(KeypointDetector):
+    def describeFeatures(self, image, keypoints):
+        '''
+        Input:
+            image -- BGR image with values between [0, 255]
+            keypoints -- the detected features, we have to compute the feature
+            descriptors at the specified coordinates
+        Output:
+            Descriptor numpy array, dimensions:
+                keypoint number x feature descriptor dimension
+        '''
+        descriptor = cv2.ORB_create()
+        kps, desc = descriptor.compute(image, keypoints)
+        if desc is None:
+            desc = np.zeros((0, 128))
+
+        return desc
+
+
+# Compute Custom descriptors (extra credit)
+class CustomFeatureDescriptor(FeatureDescriptor):
+    def describeFeatures(self, image, keypoints):
+        '''
+        Input:
+            image -- BGR image with values between [0, 255]
+            keypoints -- the detected features, we have to compute the feature
+            descriptors at the specified coordinates
+        Output:
+            Descriptor numpy array, dimensions:
+                keypoint number x feature descriptor dimension
+        '''
+        raise NotImplementedError('NOT IMPLEMENTED')
+
+
+## Feature matchers ############################################################
+
+
+class FeatureMatcher(object):
+    def matchFeatures(self, desc1, desc2):
+        '''
+        Input:
+            desc1 -- the feature descriptors of image 1 stored in a numpy array,
+                dimensions: rows (number of key points) x
+                columns (dimension of the feature descriptor)
+            desc2 -- the feature descriptors of image 2 stored in a numpy array,
+                dimensions: rows (number of key points) x
+                columns (dimension of the feature descriptor)
+        Output:
+            features matches: a list of cv2.DMatch objects
+                How to set attributes:
+                    queryIdx: The index of the feature in the first image
+                    trainIdx: The index of the feature in the second image
+                    distance: The distance between the two features
+        '''
+        raise NotImplementedError
+
+    # Evaluate a match using a ground truth homography.  This computes the
+    # average SSD distance between the matched feature points and
+    # the actual transformed positions.
+    @staticmethod
+    def evaluateMatch(features1, features2, matches, h):
+        d = 0
+        n = 0
+
+        for m in matches:
+            id1 = m.queryIdx
+            id2 = m.trainIdx
+            ptOld = np.array(features2[id2].pt)
+            ptNew = FeatureMatcher.applyHomography(features1[id1].pt, h)
+
+            # Euclidean distance
+            d += np.linalg.norm(ptNew - ptOld)
+            n += 1
+
+        return d / n if n != 0 else 0
+
+    # Transform point by homography.
+    @staticmethod
+    def applyHomography(pt, h):
+        x, y = pt
+        d = h[6]*x + h[7]*y + h[8]
+
+        return np.array([(h[0]*x + h[1]*y + h[2]) / d,
+            (h[3]*x + h[4]*y + h[5]) / d])
+
+
+class SSDFeatureMatcher(FeatureMatcher):
+    def matchFeatures(self, desc1, desc2):
+        '''
+        Input:
+            desc1 -- the feature descriptors of image 1 stored in a numpy array,
+                dimensions: rows (number of key points) x
+                columns (dimension of the feature descriptor)
+            desc2 -- the feature descriptors of image 2 stored in a numpy array,
+                dimensions: rows (number of key points) x
+                columns (dimension of the feature descriptor)
+        Output:
+            features matches: a list of cv2.DMatch objects
+                How to set attributes:
+                    queryIdx: The index of the feature in the first image
+                    trainIdx: The index of the feature in the second image
+                    distance: The distance between the two features
+        '''
+        matches = []
+        # feature count = n
+        assert desc1.ndim == 2
+        # feature count = m
+        assert desc2.ndim == 2
+        # the two features should have the type
+        assert desc1.shape[1] == desc2.shape[1]
+
+        if desc1.shape[0] == 0 or desc2.shape[0] == 0:
+            return []
+
+        # TODO 7: Perform simple feature matching.  This uses the SSD
+        # distance between two feature vectors, and matches a feature in
+        # the first image with the closest feature in the second image.
+        # Note: multiple features from the first image may match the same
+        # feature in the second image.
+        # TODO-BLOCK-BEGIN
+        for i, d in enumerate(desc1):
+            dist = np.sum(np.square(desc2 - d), axis=1)
+            smallest = np.argmin(dist)
+            m = cv2.DMatch()
+            m.queryIdx = i
+            m.trainIdx = smallest
+            m.distance = dist[smallest]
+            matches.append(m)
+
+        return matches
+
+
+class RatioFeatureMatcher(FeatureMatcher):
+    def matchFeatures(self, desc1, desc2):
+        '''
+        Input:
+            desc1 -- the feature descriptors of image 1 stored in a numpy array,
+                dimensions: rows (number of key points) x
+                columns (dimension of the feature descriptor)
+            desc2 -- the feature descriptors of image 2 stored in a numpy array,
+                dimensions: rows (number of key points) x
+                columns (dimension of the feature descriptor)
+        Output:
+            features matches: a list of cv2.DMatch objects
+                How to set attributes:
+                    queryIdx: The index of the feature in the first image
+                    trainIdx: The index of the feature in the second image
+                    distance: The ratio test score
+        '''
+        matches = []
+        # feature count = n
+        assert desc1.ndim == 2
+        # feature count = m
+        assert desc2.ndim == 2
+        # the two features should have the type
+        assert desc1.shape[1] == desc2.shape[1]
+
+        if desc1.shape[0] == 0 or desc2.shape[0] == 0:
+            return []
+
+        # TODO 8: Perform ratio feature matching.
+        # This uses the ratio of the SSD distance of the two best matches
+        # and matches a feature in the first image with the closest feature in the
+        # second image.
+        # Note: multiple features from the first image may match the same
+        # feature in the second image.
+        # You don't need to threshold matches in this function
+        for i, d in enumerate(desc1):
+            dist = np.sum(np.square(desc2 - d), axis=1)
+            first, second = np.argsort(dist)[:2]
+            m = cv2.DMatch()
+            m.queryIdx = i
+            m.trainIdx = first
+            m.distance = dist[first]/dist[second]
+            matches.append(m)
+
+        return matches
+
+
+class ORBFeatureMatcher(FeatureMatcher):
+    def __init__(self):
+        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        super(ORBFeatureMatcher, self).__init__()
+
+    def matchFeatures(self, desc1, desc2):
+        return self.bf.match(desc1.astype(np.uint8), desc2.astype(np.uint8))
